3889d2ca6fa0f11ef7206d33076f6f73792a66e4
Іван Пірус <ivan.88.verb@gmail.com>
2015-12-23 14:01:36 +0200

    I created template site, use bootstrap

---
+++ tipsops/template/footer.php
@@ -0,0 +1,11 @@
+<?php
+    echo '<footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; Your Website 2015</p>
+                </div>
+            </div>
+            <!-- /.row -->
+        </footer>
+    ';
+ ?>
