632ffed18465b95de595a944d8f6d53c9fe0cfc1
Іван Пірус <ivan.88.verb@gmail.com>
2016-01-13 17:47:02 +0200

    I fix block navigation

--- Content/Controllers/Views/block_list.php
+++ Content/Controllers/Views/block_list.php
@@ -1,11 +1,11 @@
 <?php
 function show_list($list){
     $count = count($list);
-
+        echo "<div class='btn-group-justified'>";
         for($i=0; $i<$count;  $i++)
         {
-            echo"<button type=\"button\" class=\"btn btn-info\" id='button'>$list[$i]</button>";
+            echo"<button type='button' class='btn btn-success' id='button'>$list[$i]</button>";
         }
-
+        echo"</div>";
 }
 ?>
---
+++ Content/Controllers/title.php
@@ -0,0 +1,7 @@
+<?php
+include "Models/first_languages.php";
+$list =get__firstLanguages();
+include "Views/Views_js/title.html";
+
+
+?>
