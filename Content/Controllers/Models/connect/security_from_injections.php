c3b0ea7ab2c5a0313505a202dd38dcadc03c063c
Іван Пірус <ivan.88.verb@gmail.com>
2016-01-18 12:23:35 +0200

    I add select all articles with sql ,delete bit template content, change style title and placed file protections with sql injections

---
+++ Content/Controllers/Models/connect/security_from_injections.php
@@ -0,0 +1,5 @@
+<?php
+function sip ($in)  // this function
+{
+    return "unhex('".bin2hex($in)."')";
+}
--- Content/Controllers/Models/select_articles.php
+++ Content/Controllers/Models/select_articles.php
@@ -1,11 +1,5 @@
 <?php
-$title_id = "Java";
-//get__articles($title_id);
-
-function sip ($in)  // this function
-{
-    return "unhex('".bin2hex($in)."')";
-}
+include"connect/security_from_injections.php"; //include function protections sip($in)
 
 function get__articles($title_id)
 {
@@ -13,12 +7,34 @@ function get__articles($title_id)
 
     $sql = "SELECT id, title_id, title, author,  article, updated_up, revisions FROM articles  WHERE title_id =".sip($title_id);
     $temp = $conn->query($sql); //the variable $conn connect with file connect.php
-    $temp->num_rows;
-    $result = $temp->fetch_array();
+    $number = $temp->num_rows;
+    $result = array();
+    if($number>0) {
+        for ($i = 0; $i < $number; $i++) {
+            $result[$i] = $temp->fetch_array();
+        }
+    }
+    else{die("SQL: Select is not successful");}
     // die($result[3]);
 
     $temp->close(); // free memory
     $conn->close(); // close connect
     return $result;
+}
+
+function get__numbers($article)
+{
+    include ('connect/connect.php');
+    $sql = "SELECT COUNT('id') FROM articles WHERE title_id =".sip($article);
+    $temp = $conn->query($sql); //the variable $conn connect with file connect.php
+    if($temp->num_rows>0)
+    {
+        $result =  $temp->fetch_array();
+    }
+    else{die("SQL: Select is not successful");}
+
+    $temp->close(); // free memory
+    $conn->close(); // close connect
+    return $result[0];
 }
 ?>
