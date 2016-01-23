446c08e964779f78b30930698b2745b46c191f85
Іван Пірус <ivan.88.verb@gmail.com>
2016-01-23 11:45:31 +0200

    I fix template site

---
+++ Content/scripts/search.js
@@ -0,0 +1,11 @@
+var search = new SearchAttr();
+var src = ["google"];
+var lincks = ["http"];
+var description = ["Öå Google"];
+    search.option({
+        yard: "search",
+        maxlength: 25,
+        src: src,
+        links: links,
+        de
+    })
