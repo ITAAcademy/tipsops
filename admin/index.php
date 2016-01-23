c70398d3264f69625f7b8f361a0d1e8565ceffd6
Alexander Khupchenko <khupchenko86@gmail.com>
2016-01-23 11:40:22 +0200

    add folder admin

---
+++ admin/index.php
@@ -0,0 +1,5 @@
+<?php
+    require_once("../Content/Controllers/Models/connect/connect.php");
+    require_once("../Content/Controllers/Models/select_articles.php");
+
+    $articles = get__numbers($article);
