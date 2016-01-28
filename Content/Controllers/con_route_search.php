2d5e630e7c10646193c286d70770c3020ccb5c15
Іван Пірус <ivan.88.verb@gmail.com>
2016-01-28 12:10:49 +0200

    I add function show empty result search

--- Content/Controllers/con_route_search.php
+++ Content/Controllers/con_route_search.php
@@ -2,7 +2,7 @@
 include'con_main_search.php';
 include 'controller_main_language.php';
 include'Models/select_articles.php';
-include'Views/search_numbers.php';
+include 'Views/search.php';
 
 if(isset($_GET['key']))
 {
@@ -12,10 +12,17 @@ else{ die("Nothing is found");}
 if($words === "") return false;
 $select_search = SearchSelect($words);         // get select with db
 $array_result = SearchResult($select_search); //controller get select with db
-PrepareUpdate($array_result); // update reviews article
-$result = count($array_result);
-ShowNumbers($result);
-ShowResult($array_result);
+if($array_result != false)
+{
+    PrepareUpdate($array_result); // update reviews article
+    $result = count($array_result);
+    ShowNumbers($result);
+    ShowResult($array_result);
+}
+else{
+    ShowNumbers(0);
+    ShowEmpty($words);
+}
 
 
 ?>
