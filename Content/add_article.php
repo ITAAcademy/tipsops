77bb65f0eaa435dd6b7235d8685049c80b066ef1
sbrezdeniuk87 <sbrezdeniuk@gmail.com>
2016-02-01 00:50:50 +0200

    for Data Base Last

--- Content/add_article.php
+++ Content/add_article.php
@@ -8,7 +8,7 @@ $authors = $_POST['inp2'];
 $dates = $_POST['date'];
 $editors = $_POST['editor1'];
 $resault = "INSERT INTO articles(title_id,title,author, preview_article,article, created_up)
-                        VALUES ('$catalog','$titels','$authors','$editors','$editors',$dates);";
+                        VALUES ((SELECT id FROM languages WHERE title='$catalog'),'$titels','$authors','$editors','$editors',$dates);";
 
 if(isset($_POST['add_article'])){
     mysqli_query($conn, $resault);
