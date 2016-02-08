640a261b886c3a292a5ff256bedc4d84996e297c
sbrezdeniuk87 <sbrezdeniuk@gmail.com>
2016-02-08 18:25:16 +0200

    зміна запису в базу

--- Content/add_article.php
+++ Content/add_article.php
@@ -9,11 +9,12 @@ $catalog = $_POST['sel1'];
 $titels = $_POST['inp1'];
 $authors = $_POST['inp2'];
 $dates = $_POST['date'];
-$editors = strip_tags($_POST['editor1']);
-$previe = substr($editors,0,500);
-$previe1 = substr($previe, 0, strrpos($previe, ' '));
+$editors = $_POST['editor1'];
+$previe = strip_tags($_POST['editor1']);
+$previe = substr($previe,0,500);
+$previe = substr($previe, 0, strrpos($previe, ' '));
 $resault = "INSERT INTO articles(title_id,title,author, preview_article,article, created_up)
-                        VALUES ((SELECT id FROM languages WHERE title='$catalog'),'$titels','$authors','$previe1','$editors','$dates');";
+                        VALUES ((SELECT id FROM languages WHERE title='$catalog'),'$titels','$authors','$previe','$editors','$dates');";
 
 if(isset($_POST['add_article'])){
     mysqli_query($conn, $resault);
