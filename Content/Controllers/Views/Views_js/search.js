15fe59aa8db7d999cc96e8b084dfbc6352b4132d
sbrezdeniuk87 <sbrezdeniuk@gmail.com>
2016-04-20 21:00:20 +0300

    fixed bug 443 (working button Enter)

--- Content/Controllers/Views/Views_js/search.js
+++ Content/Controllers/Views/Views_js/search.js
@@ -3,6 +3,18 @@
  * Group: B14
  * Date: January 2016
  */
+
+function PressEnter(your_text, your_event) {
+    if(your_text != "" && your_event.keyCode == 13) {
+        var emty = "Результати пошуку";
+        $('h2#language').text(emty);
+        $.get('Content/Controllers/con_route_search.php', {key: your_text}, function (data) {
+            $('div.main_content').html(data);
+        });
+    }
+};
+
+
 $(document).ready(function(){
    $('button#button_search').click(function(){
        var text = $('input#search').val();
