1e3013c68043e2b7cfdaca209cd67afcf5769297
sbrezdeniuk87 <sbrezdeniuk@gmail.com>
2016-04-23 11:24:21 +0300

    delet

--- Content/Controllers/Views/Views_js/title_start.js
+++
@@ -1,9 +0,0 @@
-/**
- * User: Ivan Pirus
- * Group: B14
- * Date: January 2016
- */
-$.get('content/controllers/controller_title.php?', {id: null}, function(data){
-    $('h2#language').html(data);
-});
-
--- Content/show_new_article.php
+++
@@ -1,8 +0,0 @@
-<?php
-    include 'controllers/models/select_articles.php';
-    include 'controllers/controller_main_language.php';
-    $head_title = NewArticle();
-    ShowNewArticles($head_title);
-    echo"<script src='controllers/views/views_js/articles.js'></script>";
-    echo"<script src='controllers/views/views_js/copy_url.js'></script>";
-?>
